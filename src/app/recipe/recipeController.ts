e18708f4eec9626fcd9f84fa228a205f7815cb86
mr-sazzad <main.sazzad@gmail.com>
2024-01-16 19:28:56 +0600

    all-recipe-controller-added

--- src/app/recipe/recipeController.ts
+++ src/app/recipe/recipeController.ts
@@ -18,3 +18,67 @@ export const createNewRecipe: RequestHandler = async (req, res, next) => {
     next(err);
   }
 };
+
+export const getAllRecipes: RequestHandler = async (_, res, next) => {
+  try {
+    const result = await recipeServices.getAllRecipes();
+
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: "all recipes retrieved successfully",
+      data: result,
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+export const getSingleRecipe: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await recipeServices.getSingleRecipe(id);
+
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: "recipe retrieved successfully",
+      data: result,
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+export const updateSingleRecipe: RequestHandler = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const result = await recipeServices.updateSingleRecipe(id, data);
+
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: "recipe updated successfully",
+      data: result,
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+export const deleteSingleRecipe: RequestHandler = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const result = await recipeServices.deleteSingleRecipe(id);
+
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: "recipe deleted successfully",
+      data: result,
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
